836c6373d0ad084299e24af287c60438983d326c
Sentinel65 <54531291+Sentinel65@users.noreply.github.com>
2021-06-24 09:19:15 +0200

    Update hand.js

--- js/hand.js
+++ js/hand.js
@@ -283,7 +283,7 @@ class CardInHand {
         }
       } else if (this.id === ISLAND) {
         var selectedCard = hand.getCardById(this.actionData[0]);
-        if (selectedCard === undefined || !(selectedCard.suit === 'Flood' || selectedCard.suit === 'Flame')) {
+        if (selectedCard === undefined || !(selectedCard.suit === 'Flood-Potopa' || selectedCard.suit === 'Flame-Oheň')) {
           this.actionData = undefined;
         } else {
           this.clearsPenalty = function(card) {
